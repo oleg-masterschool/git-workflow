0ce79e974feedd4a55b1f574148f8feba5a8eeac
oleg-masterschool <87778112+oleg-masterschool@users.noreply.github.com>
2021-11-11 18:24:53 +0200

    adds app.js file with logic

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { useState } from "react";
+
+function App() {
+    const [name, setName] = useState('');
+    const [greeting, setGreeting] = useState(false);
+
+    function onClickHandler(e) {
+        e.preventDefault();
+        setGreeting(true);
+    }
+
+    function changeHandler(e) {
+        setName(e.target.value);
+    }
+
+    return (
+        <div className="App">
+            <section>
+                <form>
+                    <label htmlFor="name">Enter your name</label>
+                    <input type="text" id="name" onChange={changeHandler} value={name}/>
+                    <button onClick={onClickHandler}>Submit</button>
+                </form>
+
+                {greeting && (
+                    <p>Welcome, {name}!</p>
+                )}
+            </section>
+        </div>
+    );
+}
+
+export default App;
